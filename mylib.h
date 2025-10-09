c69314900646e8e2a3b1a91cf4b39523503ca8c5
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-09 14:05:32 +0300

    Create mylib.h

---
+++ mylib.h
@@ -0,0 +1,4 @@
+int myatoi(const char* a);
+int minmax(int a, int b, int c, int *min, int *mid, int *max);
+int mystrcmp(const char* s1, const char* s2);
+void mystrcpy(char* dest, const char* src);
