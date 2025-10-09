dbbce3b42125dac14c882bb42c8640a8a19681e2
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-09 15:47:10 +0300

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -1,4 +1,3 @@
 int myatoi(const char* a);
-int minmax(int a, int b, int c, int *min, int *mid, int *max);
 int mystrcmp(const char* s1, const char* s2);
 void mystrcpy(char* dest, const char* src);
