87c984e0c0b1593a71ad9e0bed3cd42af105fcd8
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-10 10:38:01 +0300

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -1,3 +1,5 @@
 int myatoi(const char* a);
 int mystrcmp(const char* s1, const char* s2);
+int sortbirth(struct birthdate a, struct birthdate b);
 void mystrcpy(char* dest, const char* src);
+void printmyData(struct myData stud);
