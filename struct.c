b62092ffa9c9016f7afcfeb49ee516e938552d0b
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-10 10:40:06 +0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -16,8 +16,6 @@ struct myData
     } bdate;
 };
 
-void printmyData(struct myData stud);
-int sortbirth(struct birthdate a, struct birthdate b);
 
 int main()
 {
@@ -120,22 +118,4 @@ int main()
     return 0;
 }
 
-void printmyData(struct myData stud)
-{
-    printf("Age: %d\nName: %s\nYear: %d\nCourse: %s\nBirthdate: %d/%d/%d\n\n",
-           stud.age, stud.name, stud.year, stud.course,
-           stud.bdate.day, stud.bdate.month, stud.bdate.year);
-}
 
-int sortbirth(struct birthdate a, struct birthdate b)
-{
-    if (a.year != b.year) 
-    {
-        return a.year - b.year;
-    }    
-    if (a.month != b.month) 
-    {
-        return a.month - b.month;
-    }
-    return a.day - b.day;
-}
