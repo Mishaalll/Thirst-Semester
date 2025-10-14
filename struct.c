cb94954247cbb9506c014489330b1daaa6e4dd82
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-14 19:46:40 +0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -16,9 +16,10 @@ struct myData
     } bdate;
 };
 
-
 int main()
 {
+    int total = 3;
+
     static struct myData student[3];
     student[0].age = 17;
     student[0].name = "Misha";
@@ -44,12 +45,16 @@ int main()
     student[2].bdate.month = 12;
     student[2].bdate.year = 2002;
 
+
     int ind[3] = {0, 1, 2};
     for (int i = 0; i < 3; i++)
     {
         for (int j = i + 1; j < 3; j++)
         {
-            if (sortbirth(student[ind[i]].bdate, student[ind[j]].bdate) > 0)
+            struct birthdate a = student[ind[i]].bdate;
+            struct birthdate b = student[ind[j]].bdate;
+
+            if (a.year > b.year || (a.year == b.year && a.month > b.month) ||(a.year == b.year && a.month == b.month && a.day > b.day))
             {
                 int temp = ind[i];
                 ind[i] = ind[j];
@@ -58,37 +63,41 @@ int main()
         }
     }
 
+    printf("Students:\n");
+    for (int i = 0; i < 3; i++)
+        printf("Age: %d\nName: %s\nYear: %d\nCourse: %s\nBirthdate: %d/%d/%d\n\n",
+                student[i].age, student[i].name, student[i].year, student[i].course,
+                student[i].bdate.day, student[i].bdate.month, student[i].bdate.year);
 
-    printf("Students: \n");
+
+    printf("Sorted students by birth date:\n");
     for (int i = 0; i < 3; i++)
     {
-        printmyData(student[i]);
-    }
-
-    printf("Sorted students by birth date: \n");
-    for (int i = 0; i < 3; i++)
-    {
-        printmyData(student[ind[i]]);
-    }
-
+        printf("Age: %d\nName: %s\nYear: %d\nCourse: %s\nBirthdate: %d/%d/%d\n\n",
+                student[ind[i]].age, student[ind[i]].name, student[ind[i]].year, student[ind[i]].course,
+                student[ind[i]].bdate.day, student[ind[i]].bdate.month, student[ind[i]].bdate.year);
+    }    
     char decision;
-    printf("Do you want to enter new students? (y/n): \n");
-    scanf(" %c", &decision);
+    printf("Do you want to enter new students? (y/n):\n");
+    scanf("%c", &decision);
+
+    struct myData *database = NULL;
+
     if (decision == 'y')
     {
         int n;
-        printf(" Enter how many students you want to add: \n");
-        scanf(" %d", &n);
-        int total = n + 3;
+        printf("Enter how many students you want to add:\n");
+        scanf("%d", &n);
+        total = n + 3;
 
         struct myData *studentnew = malloc(total * sizeof(struct myData));
 
         for (int i = 0; i < 3; i++)
-        {
             studentnew[i] = student[i];
-        }
 
-        printf("Enter student data in format: age name year course day month birthyear:\n");
+        printf("Enter student data in format:\n");
+        printf("age name year course day month birthyear\n");
+
         for (int i = 3; i < total; i++)
         {
             studentnew[i].name = malloc(50 * sizeof(char));
@@ -102,20 +111,42 @@ int main()
                   &studentnew[i].bdate.year);
         }
 
-        printf("New students structure: \n");
+        printf("New students structure:\n");
         for (int i = 0; i < total; i++)
-        {
-            printmyData(studentnew[i]);
-        }
+            printf("Age: %d\nName: %s\nYear: %d\nCourse: %s\nBirthdate: %d/%d/%d\n\n",
+                    studentnew[i].age, studentnew[i].name, studentnew[i].year, studentnew[i].course,
+                    studentnew[i].bdate.day, studentnew[i].bdate.month, studentnew[i].bdate.year);
 
+
+        database = studentnew;
+    }
+    else
+    {
+        database = student; 
+    }
+
+    FILE *fp = fopen("database.txt", "a+");
+
+    for (int i = 0; i < total; i++)
+    {
+        fprintf(fp, "Age: %d, Name: %s, Year: %d, Course: %s, Birthdate: %02d/%02d/%04d\n",
+                database[i].age,
+                database[i].name,
+                database[i].year,
+                database[i].course,
+                database[i].bdate.day,
+                database[i].bdate.month,
+                database[i].bdate.year);
+    }
+    fclose(fp);
+    printf("Data saved to database.txt successfully!\n");
+
+    if (decision == 'y')
+    {
         for (int i = 3; i < total; i++)
-        {
-            free(studentnew[i].name);
-        }
-        free(studentnew);
+            free(database[i].name);
+        free(database);
     }
 
     return 0;
 }
-
-
